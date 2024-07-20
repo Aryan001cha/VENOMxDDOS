30d4809735872678aacb0cb20294942b7bc59dce
Aryan001cha <14commercearyan@gmail.com>
2024-07-20 22:21:36 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7373348949:AAFoy6YRe1P6cK9QwtxXmYLWz6J64xSbU7E')
 
 # Admin user IDs
-admin_id = ["your id"]
+admin_id = ["6144075982"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -48,10 +48,10 @@ allowed_user_ids = read_users()
 
 # Function to log command to the file
 def log_command(user_id, target, port, time):
-    admin_id = ["5588464519"]
+    admin_id = ["6144075982"]
     user_info = bot.get_chat(user_id)
     if user_info.username:
-        username = "@" + user_info.username
+        username = "@AryanXchauhan" + user_info.username
     else:
         username = f"UserID: {user_id}"
     
